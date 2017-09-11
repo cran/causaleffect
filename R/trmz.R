85e11d374aa5fb16a38b4f64a9ffcad981ebbe35
Santtu Tikka <santtuth@gmail.com>
2017-09-11 10:36:22 +0000

    version 1.3.5

--- R/trmz.R
+++ R/trmz.R
@@ -1,5 +1,7 @@
 trmz <- function(y, x, P, J, domain, w.index, D, Z, topo, tree) {
   to <- NULL
+  from <- NULL
+  description <- NULL
   d <- length(D)
   v.s <- lapply(D, function(x) get.vertex.attribute(x, "name"))
   s <- lapply(1:d, function(x) v.s[[x]][which(vertex.attributes(D[[x]])$description == "S")])
@@ -46,7 +48,7 @@ trmz <- function(y, x, P, J, domain, w.index, D, Z, topo, tree) {
   }
 
   # line 3
-  D.x.overbar <- subgraph.edges(D.causal, E(D.causal)[!to(x)], delete.vertices = FALSE)
+  D.x.overbar <- subgraph.edges(D.causal ,E(D.causal)[!(to(x) | (from(x) & (description == "U" & !is.na(description))))], delete.vertices = FALSE)
   anc.xbar <- ancestors(y, observed.graph(D.x.overbar), topo[[1]])
   w <- setdiff(setdiff(v, x), anc.xbar)
   if (length(w) != 0) {
